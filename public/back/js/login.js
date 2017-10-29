9e0ebf5b86c88036c64eac9b188560f6eb4d01ac
july <810388473@qq.com>
2017-10-29 21:23:47 +0800

    login完成

--- public/back/js/login.js
+++ public/back/js/login.js
@@ -1,8 +1,7 @@
 // 1. 用户名不能为空
 // 2. 用户密码不能为空
 // 3. 用户密码长度为6-12位
-;(function () {
-//使用表单校验插件
+$(function () {
   $("#form").bootstrapValidator({
 
     //2. 指定校验时的图标显示，默认是bootstrap风格
@@ -17,34 +16,73 @@
       //校验用户名，对应name表单的name属性
       username: {
         validators: {
+          //validators验证者
           //不能为空
           notEmpty: {
             message: '用户名不能为空'
           },
-          //长度校验
-          stringLength: {
-            min: 6,
-            max: 30,
-            message: '用户名长度必须在6到30之间'
-          },
-        }
+          //  手动为更新字段添加一个信息
+          callback:{
+            message:"用户名错误"
+          }
+        },
+
       },
-      pwd: {
+      password: {
         validators: {
           //不能为空
           notEmpty: {
             message: '密码不能为空'
           },
-          //长度校验
+          // //长度校验
           stringLength: {
             min: 6,
             max: 12,
-            message: '密码长度必须在6到30之间'
+            message: '密码长度必须在6到12之间'
           },
+          callback:{
+            message:"密码错误"
         },
+
+        }
       },
     }
   });
+  //获取表单校验实例
+  var validator = $("#form").data('bootstrapValidator');
+  // console.log(validator);
+  //初始化表单校验成功后注册表单成功功事件
+  $("#form").on('success.form.bv', function (e) {
+    e.preventDefault();
+    //使用ajax提交逻辑
+    $.ajax({
+      type:"post",
+      url:"/employee/employeeLogin",
+      data:$("#form").serialize(),
+      success:function (data) {
+       if(data.success){
+         location.href = "index.html";
+       }else{
+         if(data.error === 1000){
+           // BootstrapValidator默认在校验某个字段合法后不再重新校验，当调用其他插件或者方法可能会改变字段值时，需要重新对该字段进行校验。可以使用updateStatus(field, status, validatorName)方法更新字段的状态
+           // - NOT_VALIDATED：未校验的
+           // - VALIDATING：校验中的
+           // - INVALID ：校验失败的
+           // - VALID：校验成功的。
+          validator.updateStatus("username","INVALID","callback");
+         }
+         if(data.error === 1001){
+           validator.updateStatus("password","INVALID","callback");
+         }
+       }
+      }
+    })
+  });
+  $("[type = 'reset']").on("click",function () {
+    validator.resetForm();//重置表单，并且会隐藏所有的错误提示和图标
+  })
+})
+//使用表单校验插件
 
-})();
+
 
