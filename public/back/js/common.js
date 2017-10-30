14e3ff89fe523fa2ff7a1385d79275803fbd9663
july <810388473@qq.com>
2017-10-30 16:53:31 +0800

    index完成

---
+++ public/back/js/common.js
@@ -0,0 +1,49 @@
+/**
+ * Created by july on 2017/10/29.
+ */
+$(function () {
+  // 点击.child的上一个兄弟元素时，$(this)指的是$(".child").prev(),.chlid显示
+   $(".child").prev().on("click",function () {
+       $(this).next().slideToggle();
+   })
+  $(".lt_nav a").on("click",function () {
+      $(".lt_nav a").removeClass("new");
+    $(this).addClass("new");
+  })
+// 进度条
+  $(document).ajaxStart(function () {                    NProgress.start();
+  });
+  $(document).ajaxStop(function () {
+    setTimeout(function () {
+      NProgress.done();
+    }, 500);
+  });
+//  点击小图标，侧边栏显示和隐藏
+  $(".lt_main .main_header .icon_menu").on("click",function () {
+    console.log(11);
+    $(".lt_aside").toggleClass("new");
+    $(".lt_main ").toggleClass("new");
+  });
+  // 共用退出功能
+  $(".lt_main .icon_out").on("click",function () {
+    console.log(11);
+    $('#myModal').modal('show');
+  });
+  //点击确定按钮，跳到登录页面
+  $(".modal-footer .sure").on("click",function () {
+    // 发送一个ajax请求，告诉服务器我要退出了，服务器会清空你的session
+      $.ajax({
+        type:"get",
+        url:"/employee/employeeLogout",
+        // 不用再传data数据，只是一个button按钮
+        // data:"",
+        success:function (data) {
+          console.log(data);
+          if(data.success){
+          // .location.href是window的属性
+            window.location.href="login.html";
+          }
+        }
+      })
+  })
+})
